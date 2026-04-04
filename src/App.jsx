3ccaa2006ebebc7eb6ba2bb0792601654db089ca
Randi Sullivan <w0519995@campus.nscc.ca>
2026-04-04 10:30:02 -0300

    Added header and navbar (that does not connect anywhere). Next working on the API pull? I think

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container'
 import Header from './components/Header'
-import logo from './logo.svg'
 
 function App() {
  
   return (
-    <header>
-      <div className="header">
-        <Container>
-          <div className="holder">
-            <div className="logo">
-              <img src={logo} alt="logo" className="img-fluid w-25" />
-            </div>
-          </div>
-        </Container>
-      </div>
-    </header>
+    <>
+      <Header />
+    </>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,41 @@
-export default function Header() {
-    return (
-    <header>
-        <h1>Header For Now</h1>
+import logo from '/src/white-logo.svg';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+
+const Header = () => {
+  return (
+    <header className="site-header">
+      <div className="holder">
+        <div className="logo">
+          <img src={logo} alt="logo" style={{ width: '80px' }} />
+          <span className="site-name">Luna</span>
+        </div>
+
+        <div className="moonPhaseHeader">
+          <ul>
+            <li><span>🌙</span></li>
+            <li><span>dateplaceholder</span></li>
+          </ul>
+        </div>
+      </div>
+     <Navbar expand="lg" className="gold-navbar">
+      <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="">Home</Nav.Link>
+            <Nav.Link href="">Link</Nav.Link>
+            <Nav.Link href="">Home</Nav.Link>
+            <Nav.Link href="">Link</Nav.Link>
+            <Nav.Link href="">Home</Nav.Link>
+            <Nav.Link href="">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     </header>
-    )
+  )
 }
+
+export default Header;
