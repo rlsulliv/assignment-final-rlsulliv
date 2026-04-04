c26585102dc56a61fbe57c27900cc6d78dc1d1f3
Randi Sullivan <w0519995@campus.nscc.ca>
2026-04-04 14:35:34 -0300

    Hero section complete. Going to work on the API pull now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 
+
 const Header = () => {
   return (
     <header className="site-header">
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,16 @@
-import { Container, Row, Col } from 'react-bootstrap'
-import hero from '/src/small-hero.svg';
+import hero from '/src/small-hero.webp';
+import heroTwo from '/src/horizon-hero.webp';
 
 function Home() {
     return (
     <div>
         <section className="hero">
-            <img src={hero} alt="hero" className="hero-image"/>
+            <img src={hero} alt="hero-one" className="hero-image-one"/>
+            <img src={heroTwo} alt="hero-two" className="hero-image-two"/>
             <div className="hero-content">
-                <p className="hero-tagline">✦ your personal cosmic guide ✦</p>
+                <h2 className="hero-tagline">✦ your personal cosmic guide ✦</h2>
                 <h1 className="hero-title">Discover What The <span>Stars Say About You</span></h1>
-                <p className="hero-desc">Enter your birth details and uncover your natal chart, planetary positions, and the mysteries written in the sky at the moment you were born.</p>
+                <p className="hero-desc">Enter your birth details and uncover your natal chart, planetary positions, and the mysteries written in the sky <br /> at the moment you were born.</p>
             </div>
         </section>
     </div>
